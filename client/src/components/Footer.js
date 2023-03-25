6e24eee1c84a7ab9505e4f3da1f2a61388204d21
Atıf Şimşek <dev.atifsimsek@gmail.com>
2023-03-26 02:12:01 +0300

    backend info added

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Footer = () => {
+  const loading = useSelector(state => state.todoReducer.isLoading)
   return (
-    <footer className="info">
-      <p>Click to edit a todo</p>
-      <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
-      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-    </footer>
+    <>
+      <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+
+        {loading &&
+          <div className='info-backend'>
+            Backend may take some time to start initially, please wait a moment.
+          </div>
+        }
+      </footer>
+
+    </>
+
+
+
   )
 }
 
